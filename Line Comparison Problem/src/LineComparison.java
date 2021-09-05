19701b740eb20313fb152ee6850a63f0e9f61912
Sajan <sajanmhatre29@gmail.com>
2021-09-06 03:25:07 +0530

    Compare Lines using OOP

--- Line Comparison Problem/src/LineComparison.java
+++ Line Comparison Problem/src/LineComparison.java
@@ -16,6 +16,15 @@ public class LineComparison {
         this.x2 = x2;
         this.y2 = y2;
     }
+    //Constructor
+    public LineComparison() {
+        this.point1 = point1;
+        this.x1 = x1;
+        this.y1 = y1;
+        this.point2 = point2;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
         //Method to measure Length of Line Segments
         public double measureLength(){
 //        System.out.println("To Measure the Length ");
@@ -23,6 +32,21 @@ public class LineComparison {
 //        System.out.println("Length of Line Segment "+point1+point2+" is: "+ length);
 
             return length;
+        }
+
+        //Method to compare Line Segment
+        public void compareLines(Integer length1, Integer length2){
+            //Condition to check equality of Line Segments
+            if (length1.compareTo(length2)>0){
+                System.out.println("Length of line PQ "+length1+" is greater than "+length2+" of Line ST");
+            }
+            else if (length1.equals(length2)){
+                System.out.println("Length of line PQ "+length1+" is equal to "+length2+" of Line ST");
+            }
+            else{
+                System.out.println("Length of line PQ "+length1+" is smaller than "+length2+" of Line ST");
+            }
+
         }
         public static void main(String[] args) {
             System.out.println("Measure the Length of Line");
@@ -35,16 +59,9 @@ public class LineComparison {
             Integer length1= (int)PQ.measureLength();
 //        System.out.println("Length 1 value is: "+ length1);
             Integer length2= (int)ST.measureLength();
-            //Condition to check equality of Line Segments
-            if (length1.compareTo(length2)>0){
-                System.out.println("Length of line PQ "+length1+" is greater than "+length2+" of Line ST");
-            }
-            else if (length1.equals(length2)){
-                System.out.println("Length of line PQ "+length1+" is equal to "+length2+" of Line ST");
-            }
-            else{
-                System.out.println("Length of line PQ "+length1+" is smaller than "+length2+" of Line ST");
-            }
+            //Object
+            LineComparison check = new LineComparison();
+            check.compareLines(length1,length2);
 
         }
     }
