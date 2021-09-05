07b60315b952d7f355625b433d2f5cba7861b245
Sajan <sajanmhatre29@gmail.com>
2021-09-06 03:19:11 +0530

    Compare Lengths of Lines

--- Line Comparison Problem/src/LineComparison.java
+++ Line Comparison Problem/src/LineComparison.java
@@ -1,25 +1,25 @@
 public class LineComparison {
+    //Instance Variables
+     String point1;
+     Integer x1;
+     Integer y1;
+     String point2;
+     Integer x2;
+     Integer y2;
 
-        private final String point1;
-        private final double x1;
-        private final double y1;
-        private final String point2;
-        private final double x2;
-        private final double y2;
-
-        public LineComparison(String point1, double x1, double y1, String point2, double x2, double y2) {
-            this.point1 = point1;
-            this.x1 = x1;
-            this.y1 = y1;
-            this.point2 = point2;
-            this.x2 = x2;
-            this.y2 = y2;
-        }
-
-        //Method
+    //Constructor
+    public LineComparison(String point1, Integer x1, Integer y1, String point2, Integer x2, Integer y2) {
+        this.point1 = point1;
+        this.x1 = x1;
+        this.y1 = y1;
+        this.point2 = point2;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+        //Method to measure Length of Line Segments
         public double measureLength(){
 //        System.out.println("To Measure the Length ");
-            double length = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+            double length = (int)(Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2)));
 //        System.out.println("Length of Line Segment "+point1+point2+" is: "+ length);
 
             return length;
@@ -32,23 +32,19 @@ public class LineComparison {
             //Method
             PQ.measureLength();
             ST.measureLength();
-            double length1= PQ.measureLength();
+            Integer length1= (int)PQ.measureLength();
 //        System.out.println("Length 1 value is: "+ length1);
-            double length2= ST.measureLength();
-//        System.out.println("Length 2 value is: "+ length2);
-//        String PQ="45";
-//        String ST="65";
-//        int ST= 67;
-            if (length1==length2){
+            Integer length2= (int)ST.measureLength();
+            //Condition to check equality of Line Segments
+            if (length1.compareTo(length2)>0){
+                System.out.println("Length of line PQ "+length1+" is greater than "+length2+" of Line ST");
+            }
+            else if (length1.equals(length2)){
                 System.out.println("Length of line PQ "+length1+" is equal to "+length2+" of Line ST");
             }
             else{
-                System.out.println("Length of line PQ "+length1+" is not equal to "+length2+" of Line ST");
+                System.out.println("Length of line PQ "+length1+" is smaller than "+length2+" of Line ST");
             }
-
-
-
-
 
         }
     }
